8c6fa584d45a964e6a80383e332106dfcf6b3657
rlarkdmf1105 <rlarkdmf1105@gmail.com>
2023-11-09 15:45:50 +0900

    재제출

--- likeapp/views.py
+++ likeapp/views.py
@@ -45,3 +45,29 @@ def delete(request, id):
     blog = get_object_or_404(Blog, id = id)
     blog.delete()
     return redirect('read')
+
+def like(request, id):
+    if request.user.is_authenticated:
+        blog = get_object_or_404(Blog, id=id)
+        if blog.like_user.filter(id=request.user.id).exists():
+            blog.like_user.remove(request.user)
+        else:
+            blog.like_user.add(request.user)
+        return redirect('detail', id=id)
+    return redirect('login')
+
+def bookmark(request, id):
+    if request.user.is_authenticated:
+        blog = get_object_or_404(Blog, id = id)
+        if blog.bookmark_user.filter(id = request.user.id).exists():
+            blog.bookmark_user.remove(request.user)
+        else:
+            blog.bookmark_user.add(request.user)
+        return redirect('detail', id=id)
+    return redirect('login')
+
+def mybookmark(request):
+    if request.user.is_authenticated:
+        blogs = Blog.objects.filter(bookmark_user=request.user)
+        return render(request, 'mybookmark.html', {'blogs' : blogs})
+    return redirect('login')
